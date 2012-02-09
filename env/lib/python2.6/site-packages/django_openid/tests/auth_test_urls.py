d56883704b0943c90f8e6f23639ef6fe4ebac034
Fredrick Galoso <fred@wayoutmind.com>
2012-02-08 20:01:50 -0600

    clean up repo

--- env/lib/python2.6/site-packages/django_openid/tests/auth_test_urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls.defaults import *
-from django.http import HttpResponse
-from django_openid.registration import RegistrationConsumer
-
-urlpatterns = patterns('',
-    (r'^$', lambda r: HttpResponse('Index')),
-    (r'^openid/(.*)', RegistrationConsumer()),
-)
