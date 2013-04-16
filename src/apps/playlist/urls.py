295e751774df02f9d9a5b6634c4bb2983fea0b14
dmur <dolan.murphy@gmail.com>
2013-04-16 10:02:55 -0500

    remove dev code from master branch

--- src/apps/playlist/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf import settings
-from django.conf.urls.defaults import *
-
-urlpatterns = patterns("",
-    url(r"^$", 'playlist.views.default'),
-)
--- src/apps/playlist/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import *
-from playlist.models import *
-
-def default(request):
-    return render_to_response('playlist.html',{})
